aa24dd1fdc697f04fc8b78ff1f4227326cd48561
Abe Enoch Aboluwarin <60470721+abeenoch@users.noreply.github.com>
2022-07-16 17:39:27 +0100

    Add files via upload

---
+++ students/src/test/java/com/oshinaikecodes/students/StudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshinaikecodes.students;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
